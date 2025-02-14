a38f2572e68ed181eedd71919aac46939816ef18
theQuarky <ranahiren27@gmail.com>
2025-02-15 04:20:06 +0530

    all types of var declaration fixed from variable.tspp

--- ts++/src/parser/visitors/parse_visitor/declaration/declaration_parse_visitor.h
+++ ts++/src/parser/visitors/parse_visitor/declaration/declaration_parse_visitor.h
@@ -1,22 +1,22 @@
 #pragma once
 #include "class_decl_visitor.h"
 #include "func_decl_visitor.h"
-#include "parser/nodes/declaration_nodes.h"
-#include "var_decl_visitor.h"
 #include "ideclaration_visitor.h"
+#include "parser/nodes/declaration_nodes.h"
 #include "parser/visitors/parse_visitor/expression/iexpression_visitor.h"
+#include "var_decl_visitor.h"
 
 namespace visitors {
 
 class ExpressionParseVisitor;
 class StatementParseVisitor;
 
-class DeclarationParseVisitor: public IDeclarationVisitor {
+class DeclarationParseVisitor : public IDeclarationVisitor {
 public:
   DeclarationParseVisitor(tokens::TokenStream &tokens,
                           core::ErrorReporter &errorReporter,
                           IExpressionVisitor &exprVisitor,
-                          IStatementVisitor& stmtVisitor);
+                          IStatementVisitor &stmtVisitor);
 
   nodes::DeclPtr parseDeclaration() override;
   nodes::TypePtr parseType() override;
@@ -28,11 +28,12 @@ private:
   bool check(tokens::TokenType type) const;
   void error(const std::string &message);
   bool consume(tokens::TokenType type, const std::string &message);
+  
   // Resources
   tokens::TokenStream &tokens_;
   core::ErrorReporter &errorReporter_;
   IExpressionVisitor &exprVisitor_;
-  IStatementVisitor& stmtVisitor_;
+  IStatementVisitor &stmtVisitor_;
 
   // Sub-visitors
   VariableDeclarationVisitor varDeclVisitor_;
@@ -43,5 +44,14 @@ private:
   tokens::TokenType parseStorageClass();
   std::vector<nodes::AttributePtr> parseAttributeList();
   nodes::AttributePtr parseAttribute();
+  nodes::TypePtr parsePrimaryType();
+  nodes::TypePtr parseTemplateType(const core::SourceLocation &location);
+  nodes::TypePtr parseSmartPointerType(const core::SourceLocation &location);
+  nodes::TypePtr parseUnionType(nodes::TypePtr leftType,
+                                const core::SourceLocation &location);
+  nodes::TypePtr parseArrayType(nodes::TypePtr elementType,
+                                const core::SourceLocation &location);
+  nodes::TypePtr parsePointerType(nodes::TypePtr baseType,
+                                  const core::SourceLocation &location);
 };
 } // namespace visitors
